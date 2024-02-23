9be45c92a3dc901b8471b0fd1fdb7460fd900dfd
REPSOLL <yanamyh2005@gmail.com>
2024-02-23 16:21:49 +0200

    initial commit

---
+++ VirtualPetAdoptionCenter.Core/DIConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VirtualPetAdoptionCenter.Models.Configuration;
+
+namespace VirtualPetAdoptionCenter.Core
+{
+    public static class DIConfiguration
+    {
+        public static void RegisterCoreDependencies(this IServiceCollection services)
+        {
+          
+        }
+
+        public static void RegisterCoreConfiguration(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.Configure<AppConfig>(configuration.GetSection("AppConfig"));
+        }
+    }
+}
