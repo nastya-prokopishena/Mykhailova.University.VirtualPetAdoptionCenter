552fd9754ad623564a0fff7820cfd5f2dbcea9da
REPSOLL <yanamyh2005@gmail.com>
2024-02-27 16:42:16 +0200

    AAD

---
+++ VirtualPetAdoptionCenter.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualPetAdoptionCenter.Web.Controllers
+{
+    [ApiController]
+    [Route("/api/authorization")]
+    public class AuthorizationController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Get")]
+        public ActionResult Get()
+        {
+            return null;
+        }
+    }
+}
